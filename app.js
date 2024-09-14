285f426675aaba088e9135d400713bb7457cdb99
SafiaKhalid <119669832+SafiaKhalid@users.noreply.github.com>
2024-09-14 15:26:57 +0100

    Added dragover/drop event listeners to image-container element in app.js, added getImage function to reduce duplication

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const browseBtn_input = document.getElementById('browse-btn');
 const imageOverlay_div = document.getElementById('image-overlay');
+const imageContainer_section = document.getElementById('image-container');
+
+imageContainer_section.addEventListener('dragover', (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+});
+
+imageContainer_section.addEventListener('drop', (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const localImg = e.dataTransfer.files[0];
+    getImage(localImg);
+});
 
 browseBtn_input.addEventListener('change', (e) => {
     const localImg = e.target.files[0];
+    getImage(localImg);
+});
+
+const getImage = (image) => {
     const reader = new FileReader();
 
     reader.addEventListener(
@@ -13,12 +31,12 @@ browseBtn_input.addEventListener('change', (e) => {
         false
     );
 
-    if (localImg) {
-        reader.readAsDataURL(localImg);
+    if (image) {
+        reader.readAsDataURL(image);
     }
 
     imageOverlay_div.classList.remove('below-stack');
-});
+};
 
 //Drag-drop
 //Allow url upload
