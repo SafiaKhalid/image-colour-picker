e9c385a0e83e65e7adaaf8185b82465adbe9094a
SafiaKhalid <119669832+SafiaKhalid@users.noreply.github.com>
2024-09-14 15:08:05 +0100

    Select image locally in app.js when select browse-btn, set image-overlay to selected image, remove class to have image on top of stack

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const browseBtn_input = document.getElementById('browse-btn');
+const imageOverlay_div = document.getElementById('image-overlay');
 
-browseBtn_input.addEventListener('click', () => {
-    console.log('press');
+browseBtn_input.addEventListener('change', (e) => {
+    const localImg = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener(
+        'load',
+        () => {
+            imageOverlay_div.style.backgroundImage = `url(${reader.result})`;
+        },
+        false
+    );
+
+    if (localImg) {
+        reader.readAsDataURL(localImg);
+    }
+
+    imageOverlay_div.classList.remove('below-stack');
 });
+
+/* imageContainer_section.style.backgroundColor = 'red'; */
